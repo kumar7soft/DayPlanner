61cca2211fe74b6283d40a13bfec37b9849b5a2d
kumar7soft <50258937+kumar7soft@users.noreply.github.com>
2022-08-17 23:06:49 -0700

    Add files via upload

---
+++ PlanningAlgorithm/src/test/java/PlanningAlgorithm/PlanningAlgorithm/PlanningAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package PlanningAlgorithm.PlanningAlgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
